f8edbfe262d40936caa96afee5790544a94f66bb
Ken Sodemann <ken@ionic.io>
2022-05-23 10:22:16 -0500

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.csdemosecurestorageng',
+  appName: 'cs-demo-secure-storage-ng',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
